21f1a0c5ed7bcfb553dbb499285cc4b2e3dd2030
Alex Temina <AlexTemina@gmail.com>
2024-03-02 01:48:26 +0100

    make clothes and others transform independent from godoth

--- _Features/rooms/living_room/living_room.gd
+++ _Features/rooms/living_room/living_room.gd
@@ -3,7 +3,9 @@ class_name LivingRoom extends Node3D
 @onready var closet = $Closet as MeshInstance3D
 @onready var closet_activable = $Closet/Activable as Activable
 
-@onready var clothes = $Closet/Clothes as Node3D
+@onready var clothes = $Closet/Node/Clothes as Node3D
+
+var cloth_names = ["underwear", "pants", "tshirt"]
 
 
 func _ready() -> void:
@@ -22,8 +24,8 @@ func make_closet_disappear() -> void:
 
 
 func reset_closet() -> void:
-	for cloth in ["underwear", "pants", "tshirt"]:
-		var cloth_activable = get_node("Closet/Clothes/" + cloth) as Node3D
+	for cloth_name in cloth_names:
+		var cloth_activable = get_node(_clothes_path(cloth_name, false)) as Node3D
 		cloth_activable.visible = true
 	clothes.visible = false
 	closet_activable.reactivate()
@@ -33,13 +35,17 @@ func make_clothes_appear() -> void:
 	print("yeah appear them")
 	clothes.visible = true
 
-	for cloth in ["underwear", "pants", "tshirt"]:
-		var cloth_activable = get_node("Closet/Clothes/" + cloth + "/Activable") as Activable
+	for cloth_name in cloth_names:
+		var cloth_activable = get_node(_clothes_path(cloth_name)) as Activable
 		cloth_activable.state_machine.transition_to(cloth_activable.state_idle.name)
 
 
 func make_cloth_disappear(cloth_name: String) -> void:
-	get_node("Closet/Clothes/" + cloth_name).visible = false
+	get_node(_clothes_path(cloth_name, false)).visible = false
 
-	var clothe_activable = get_node("Closet/Clothes/" + cloth_name + "/Activable") as Activable
+	var clothe_activable = get_node(_clothes_path(cloth_name)) as Activable
 	clothe_activable.state_machine.transition_to(clothe_activable.state_deactivated.name)
+
+
+func _clothes_path(cloth: String, with_activable := true) -> String:
+	return "Closet/Node/Clothes/" + cloth + ("/Activable" if with_activable else "")
