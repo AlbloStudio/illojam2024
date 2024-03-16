db6cb831434fd8a25112b35be653daa1d07c5d10
FisRuRadJohn <fisrujohnhen2@gmail.com>
2024-03-16 22:39:17 +0100

    tablet and ui progress bar

--- _Features/activables/tablet/tablet.gd
+++ _Features/activables/tablet/tablet.gd
@@ -1,9 +1,9 @@
 class_name Tablet extends Node
 
-@onready var tablet_screen := $TabletScreen as Control
 
+@onready var speech_bubble_label := $SpeechBubble/SpeechBubbleLabel as Label3D
 
 func say(text: String, delay := 3.0) -> void:
-	# speech_bubble_label.visible = true
-	# speech_bubble_label.text = text
-	# create_tween().tween_callback(func(): speech_bubble_label.visible = false).set_delay(delay)
+	speech_bubble_label.visible = true
+	speech_bubble_label.text = text
+	create_tween().tween_callback(func(): speech_bubble_label.visible = false).set_delay(delay)
